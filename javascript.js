5ec8258cb9e5c9fbd3cbea436703dc4c64ab442f
Gabriel Franco <francoxgabriel@gmail.com>
2022-06-16 00:19:38 -0500

    Add gradual darkening to draw squares

--- javascript.js
+++ javascript.js
@@ -8,16 +8,29 @@ function addDivs(gridSize){
     }
 }
 
-function addHoveredClass(e){
-    this.classList.add('hovered')
+function darkenSquares(e){
+    let bgCol = this.style.backgroundColor;
+    let alpha = parseFloat(bgCol.split(',')[3]); //using RGB alteration to darken squares because it does not alter div borders
+    if (alpha){
+        if (alpha >= 0.9){
+            alpha = 0.99; //cannot set to 1 as it will essesntially delete the alpha value, resetting the opacity to NaN
+        }else{
+            alpha += 0.1; //increment alpha in increments of 0.1 if it has not reached max value
+        }
+    }else{
+        alpha = 0.1; //if alpha is NaN, set it to 0.1
+    }
+    this.style.backgroundColor = `rgb(0, 0, 0, ${alpha})`;
+
 }
+
 
 function drawGrid(){
     let gridSize = prompt('What\'s the size of your grid?');
     addDivs(gridSize);
 
     const drawSquares = document.querySelectorAll('.drawSquare');
-    drawSquares.forEach(square => square.addEventListener('mouseover', addHoveredClass))
+    drawSquares.forEach(square => square.addEventListener('mouseover', darkenSquares))
 }
 
 function clearPad(){
